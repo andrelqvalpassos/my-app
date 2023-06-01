bbfd2bb650377ea0f100d5b687aa1852b1d4c7f9
Andre Luiz <andrelqvalpassos@fac.pe.senac.br>
2023-06-01 15:02:40 -0300

    att1

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
